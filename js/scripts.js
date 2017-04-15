e26f6e11bd4d87621f9fbf22a41d8567d883ec73
Laura_Hamilton <laurahamilton9@gmail.com>
2017-04-15 14:15:04 -0700

    add workshop project to git

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+alert("Hiya Monkey Pants!");
+$(document).ready(function() {
+  $("#goal-button").click(function() {
+    $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+    $("#new-goal").val("");
+  })
+})
